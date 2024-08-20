16dc40688315be7e131589e8a820eaa0e5e5ee4f
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2024-08-20 22:24:10 +0900

    🎨 update fetcher

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,1 +1,2 @@
-export const fetcher = <T>(url: string): Promise<T> => fetch(url).then((res) => res.json() as T);
+export const fetchWithToken = <T>(url: string, token: string): Promise<T> =>
+  fetch(url, { headers: { Authorization: `Bearer ${token}` } }).then((res) => res.json() as T);
